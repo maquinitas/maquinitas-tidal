0b80d8cb776d816828df48cdc43a66319c62406a
velouria <montoyamoraga@gmail.com>
2020-01-15 01:42:49 -0500

    add oto bam

---
+++ src/OtoBam.hs
@@ -0,0 +1,17 @@
+-- maquinitas-tidal
+-- parameters for oto bam
+
+module OtoBam where
+  -- cc
+  size = 12
+  preDelay = 13
+  mix = 14
+  reverbTime = 15
+  damping = 16
+  active = 17
+  type = 18
+  inGain = 19
+  lowCut = 20
+  highCut = 21
+  chorus = 22
+  freeze = 23
