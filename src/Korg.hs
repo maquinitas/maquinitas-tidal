bc88f9748ccd0933c45cde1be736879432295d08
montoyamoraga <montoyamoraga@gmail.com>
2020-04-20 15:45:20 -0400

    delete old code for testing and add Korg.hs

---
+++ src/Korg.hs
@@ -0,0 +1,32 @@
+-- maquinitas-tidal
+-- parameters for Korg instruments
+
+module Korg where
+
+  volca = [beats, fm]
+
+
+
+  -- notes
+  bd = 35
+  rs = 37
+  sd = 38
+  cp = 39
+  cy = 40
+  lt = 41
+  chh = 42
+  ohh = 46
+  ht = 47
+  cr = 49
+  rd = 51
+  lc = 52
+  hc = 53
+  ta = 54
+  cb = 56
+  ti = 57
+  ca = 58
+  la = 61
+  wb = 62
+  ha = 63
+  mp = 64
+  mf = 65
